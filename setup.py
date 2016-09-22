e9efd256d90d57be18054cd2d5dbcb11c64c333b
Pascal Corpet <pascal+github@corpet.net>
2016-09-23 00:36:04 +0200

    Add info to setup.py to automatically populate the info on PyPi.

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     version='0.3.1',
     packages=['airtable'],
     install_requires=['requests>=2.5.3'],
+    description='Python client library for AirTable',
+    author='Nicolo Canali De Rossi, Pascal Corpet',
+    url='https://github.com/bayesimpact/airtable-python',
+    keywords=['airtable', 'api'],
+    license='The MIT License (MIT)',
 )
