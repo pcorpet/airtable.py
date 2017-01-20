77c413668604cb7057a14eac2ec8537956d16479
Jan Hagelauer <jan@geekho.asia>
2017-01-20 15:15:53 +0700

    fixed path joining error on windows

--- airtable/airtable.py
+++ airtable/airtable.py
@@ -1,5 +1,5 @@
 import json
-import os
+import posixpath 
 import requests
 from collections import OrderedDict
 
@@ -40,14 +40,14 @@ def create_payload(data):
 class Airtable(object):
     def __init__(self, base_id, api_key):
         self.airtable_url = API_URL % API_VERSION
-        self.base_url = os.path.join(self.airtable_url, base_id)
+        self.base_url = posixpath.join(self.airtable_url, base_id)
         self.headers = {'Authorization': 'Bearer %s' % api_key}
 
     def __request(self, method, url, params=None, payload=None):
         if method in ['POST', 'PUT', 'PATCH']:
             self.headers.update({'Content-type': 'application/json'})
         r = requests.request(method,
-                             os.path.join(self.base_url, url),
+                             posixpath.join(self.base_url, url),
                              params=params,
                              data=payload,
                              headers=self.headers)
@@ -68,7 +68,7 @@ class Airtable(object):
             filter_by_formula=None, view=None):
         params = {}
         if check_string(record_id):
-            url = os.path.join(table_name, record_id)
+            url = posixpath.join(table_name, record_id)
         else:
             url = table_name
             if limit and check_integer(limit):
@@ -123,19 +123,19 @@ class Airtable(object):
 
     def update(self, table_name, record_id, data):
         if check_string(table_name) and check_string(record_id):
-            url = os.path.join(table_name, record_id)
+            url = posixpath.join(table_name, record_id)
             payload = create_payload(data)
             return self.__request('PATCH', url,
                                   payload=json.dumps(payload))
 
     def update_all(self, table_name, record_id, data):
         if check_string(table_name) and check_string(record_id):
-            url = os.path.join(table_name, record_id)
+            url = posixpath.join(table_name, record_id)
             payload = create_payload(data)
             return self.__request('PUT', url,
                                   payload=json.dumps(payload))
 
     def delete(self, table_name, record_id):
         if check_string(table_name) and check_string(record_id):
-            url = os.path.join(table_name, record_id)
+            url = posixpath.join(table_name, record_id)
             return self.__request('DELETE', url)
