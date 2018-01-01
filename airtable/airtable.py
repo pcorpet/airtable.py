42367ca3d8126a7c221cb3b8e8b7a2d2772ebf98
Andrey Tatarinov <elephantropy@gmail.com>
2018-01-01 22:06:26 +0300

    fix HTTPError in Python 3

--- airtable/airtable.py
+++ airtable/airtable.py
@@ -87,7 +87,7 @@ class Airtable(object):
                 message = None
                 r.raise_for_status()
             except requests.exceptions.HTTPError as e:
-                message = e.message
+                message = e
             return {
                 'error': dict(code=r.status_code, message=message)
             }
