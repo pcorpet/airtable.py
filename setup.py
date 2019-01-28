a5002e60afa69a959349a236b47b1b21ae448a3f
Joey Best-James <joseph@bestjames.com>
2019-01-28 09:52:57 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     package_data={'airtable': ['py.typed', '*.pyi']},
     install_requires=['requests>=2.5.3'],
     description='Python client library for AirTable',
-    author='Nicolo Canali De Rossi, Pascal Corpet',
-    url='https://github.com/bayesimpact/airtable-python',
+    author='Joseph Best-James, Nicolo Canali De Rossi, Pascal Corpet',
+    url='https://github.com/josephbestjames/airtable.py',
     keywords=['airtable', 'api'],
     license='The MIT License (MIT)',
 )
