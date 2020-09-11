8748cab07951f64d4d11f37e70398df0d3aa715d
Cyrille <cyrille@bayesimpact.org>
2020-09-11 13:47:30 +0200

    Strong type the airtable record dict.

--- airtable/airtable.pyi
+++ airtable/airtable.pyi
@@ -1,4 +1,5 @@
 import typing
+from typing import Any, TypedDict
 
 API_URL: str
 API_VERSION: str
@@ -9,22 +10,32 @@ class IsNotInteger(Exception):
 class IsNotString(Exception):
     ...
 
-def check_integer(n: typing.Any) -> bool:
+def check_integer(n: Any) -> bool:
     ...
 
-def check_string(s: typing.Any) -> bool:
+def check_string(s: Any) -> bool:
     ...
 
-def create_payload(data: typing.Dict[str, typing.Any]) \
-        -> typing.Dict[str, typing.Dict[str, typing.Any]]:
+def create_payload(data: Dict[str, Any]) -> Dict[str, Dict[str, Any]]:
     ...
 
-_Record = typing.Dict[str, typing.Union[str, typing.Dict[str, typing.Any]]]
 
+class _Record(TypedDict):
+    id: str
+    createdTime: str
+    fields: Dict[str, Any]
+
+
+class _DeletedRecord(TypedDict):
+    deleted: bool
+    id: str
+
+
+# TODO(cyrille): Add Generic[RecordType] for more information on what's in a record.
 class Airtable(object):
     airtable_url: str = ...
     base_url: str = ...
-    headers: typing.Dict[str, str] = ...
+    headers: Dict[str, str] = ...
 
     def __init__(self, base_id: str, api_key: str) -> None:
         ...
@@ -33,42 +44,41 @@ class Airtable(object):
             self,
             table_name: str,
             batch_size: int = 0,
-            filter_by_formula: typing.Optional[str] = None,
-            view: typing.Optional[str] = None) -> typing.Iterator[_Record]:
+            filter_by_formula: Optional[str] = None,
+            view: Optional[str] = None) -> Iterator[_Record]:
         ...
 
-    @typing.overload
+    @overload
     def get(
             self,
             table_name: str,
             record_id: None = None,
             limit: int = 0,
-            offset: typing.Optional[int] = None,
-            filter_by_formula: typing.Optional[str] = None,
-            view: typing.Optional[str] = None) \
-            -> typing.Dict[str, typing.List[_Record]]:
+            offset: Optional[int] = None,
+            filter_by_formula: Optional[str] = None,
+            view: Optional[str] = None) -> Dict[str, List[_Record]]:
      ...
 
-    @typing.overload
+    @overload
     def get(
             self,
             table_name: str,
             record_id: str,
             limit: int = 0,
-            offset: typing.Optional[int] = None,
-            filter_by_formula: typing.Optional[str] = None,
-            view: typing.Optional[str] = None) \
+            offset: Optional[int] = None,
+            filter_by_formula: Optional[str] = None,
+            view: Optional[str] = None) \
             -> _Record:
         ...
 
-    def create(self, table_name: str, data: typing.Dict[str, typing.Any]) -> _Record:
-        ...
- 
-    def update(self, table_name: str, record_id: str, data: typing.Dict[str, typing.Any]) -> _Record:
+    def create(self, table_name: str, data: Dict[str, Any]) -> _Record:
         ...
 
-    def update_all(self, table_name: str, record_id: str, data: typing.Dict[str, typing.Any]) -> _Record:
+    def update(self, table_name: str, record_id: str, data: Dict[str, Any]) -> _Record:
         ...
 
-    def delete(self, table_name: str, record_id: str) -> typing.Dict[str, typing.Union[str, bool]]:
+    def update_all(self, table_name: str, record_id: str, data: Dict[str, Any]) -> _Record:
+        ...
+
+    def delete(self, table_name: str, record_id: str) -> _DeletedRecord:
         ...
