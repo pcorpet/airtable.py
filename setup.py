cab9598b68fb8b3cbbe7b65e6d40812fdca62ea7
Pascal Corpet <pcorpet@users.noreply.github.com>
2021-01-30 00:43:50 +0100

    Migrate distribution to use pbr and deploy to PyPi in CI/CD when a new (#42)
    tag is created.

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup
-setup(
-    name='airtable',
-    version='0.4.2',
-    packages=['airtable'],
-    package_data={'airtable': ['py.typed', '*.pyi']},
-    install_requires=['requests>=2.20.0', 'six'],
-    description='Python client library for AirTable',
-    author='Joseph Best-James, Nicolo Canali De Rossi, Pascal Corpet',
-    url='https://github.com/josephbestjames/airtable.py',
-    keywords=['airtable', 'api'],
-    license='The MIT License (MIT)',
-)
+
+setup(setup_requires=["pbr"], pbr=True)
