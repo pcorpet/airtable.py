3a501579094bb0fbd1cf30a48a2228eedf42914c
Pascal Corpet <pascal@bayesimpact.org>
2021-04-02 23:19:38 +0200

    Use requests_mock to clean up tests.

--- test_airtable.py
+++ test_airtable.py
@@ -1,8 +1,7 @@
 from typing import Any, Dict
 import unittest
 
-import mock
-import requests
+import requests_mock
 
 import airtable
 
@@ -17,7 +16,7 @@ class TestAirtable(unittest.TestCase):
         self.api_key = FAKE_API_KEY
         self.airtable = airtable.Airtable(self.base_id, self.api_key)
 
-    def get(self, *args, **kwargs):
+    def get(self, *args, **kwargs):  # pylint: disable=invalid-name
         return self.airtable.get(FAKE_TABLE_NAME, *args, **kwargs)
 
     def delete(self, *args, **kwargs):
@@ -34,11 +33,9 @@ class TestAirtable(unittest.TestCase):
         self.assertEqual(self.airtable.headers['Authorization'],
                          'Bearer fake_api_key')
 
-    @mock.patch.object(requests, 'request')
-    def test_get_all(self, mock_request):
-        mock_response = mock.MagicMock()
-        mock_response.status_code = 200
-        mock_response.json.return_value = {
+    @requests_mock.mock()
+    def test_get_all(self, mock_requests):
+        mock_requests.get('https://api.airtable.com/v0/app12345/TableName', json={
             'records': [
                 {
                     'id': 'reccA6yaHKzw5Zlp0',
@@ -56,20 +53,16 @@ class TestAirtable(unittest.TestCase):
                 }
             ],
             'offset': 'reccg3Kke0QvTDW0H'
-        }
-        mock_request.return_value = mock_response
+        })
         res = self.get()
         self.assertEqual(len(res['records']), 2)
         self.assertEqual(res['offset'], 'reccg3Kke0QvTDW0H')
 
-    @mock.patch.object(requests, 'request')
-    def test_order_of_fields_is_preserved(self, mock_request):
-        mock_response = requests.Response()
-        mock_response.status_code = 200
-
+    @requests_mock.mock()
+    def test_order_of_fields_is_preserved(self, mock_requests):
         # Set the text content of the response to a JSON string so we can test
         # how it gets deserialized
-        mock_response._content = b'''{
+        mock_requests.get('https://api.airtable.com/v0/app12345/TableName', text='''{
             "records": [
                 {
                     "id": "reccA6yaHKzw5Zlp0",
@@ -108,84 +101,69 @@ class TestAirtable(unittest.TestCase):
                     }
                 }
             ]
-        }'''
-        mock_response.encoding = 'utf-8'
+        }''')
 
-        mock_request.return_value = mock_response
         response = self.airtable.get(FAKE_TABLE_NAME)
         self.assertEqual(list(response['records'][0]['fields'].keys()), list(u'abcdefghijklm'))
         self.assertEqual(list(response['records'][1]['fields'].keys()), list(u'nopqrstuvwxyz'))
 
-    @mock.patch.object(requests, 'request')
-    def test_get_by_id(self, mock_request):
+    @requests_mock.mock()
+    def test_get_by_id(self, mock_requests):
         fake_id = 'reccA6yaHKzw5Zlp0'
-        mock_response = mock.MagicMock()
-        mock_response.status_code = 200
-        mock_response.json.return_value = {
+        mock_requests.get('https://api.airtable.com/v0/app12345/TableName/reccA6yaHKzw5Zlp0', json={
             'id': 'reccA6yaHKzw5Zlp0',
             'fields': {
                 'Name': 'John',
                 'Number': '(987) 654-3210'
             }
-        }
-        mock_request.return_value = mock_response
+        })
         response = self.get(fake_id)
         self.assertEqual(response['id'], fake_id)
 
-    @mock.patch.object(requests, 'request')
-    def test_get_not_found(self, mock_request):
-        mock_response = mock.MagicMock()
-        mock_response.status_code = 404
-        mock_response.json.return_value = {
-            'error': {
-                'type': 'TABLE_NOT_FOUND',
-                'message': 'Could not find table %s in application %s' % (
-                    FAKE_TABLE_NAME, FAKE_BASE_ID),
-            },
-        }
-        mock_request.return_value = mock_response
+    @requests_mock.mock()
+    def test_get_not_found(self, mock_requests):
+        mock_requests.get(
+            'https://api.airtable.com/v0/app12345/TableName/123', status_code=404,
+            json={
+                'error': {
+                    'type': 'TABLE_NOT_FOUND',
+                    'message': 'Could not find table %s in application %s' % (
+                        FAKE_TABLE_NAME, FAKE_BASE_ID),
+                },
+            })
         with self.assertRaises(airtable.AirtableError) as error:
             self.get('123')
         self.assertEqual('TABLE_NOT_FOUND', error.exception.type)
 
-    @mock.patch.object(requests, 'request')
-    def test_get_view(self, mock_request):
-        mock_response = mock.MagicMock()
-        mock_response.status_code = 200
-        mock_response.json.return_value = {'records': []}
-        mock_request.return_value = mock_response
+    @requests_mock.mock()
+    def test_get_view(self, mock_requests):
+        mock_requests.get(
+            'https://api.airtable.com/v0/app12345/TableName?view=My+view',
+            request_headers={'Authorization': 'Bearer fake_api_key'},
+            json={'records': []})
 
         self.get(view='My view')
-        mock_request.assert_called_once_with(
-            'GET', 'https://api.airtable.com/v0/app12345/TableName',
-            data=None, headers={'Authorization': 'Bearer fake_api_key'},
-            params={'view': 'My view'})
+        self.assertEqual(1, mock_requests.called)
 
-    @mock.patch.object(requests, 'request')
-    def test_get_only_one_field(self, mock_request):
-        mock_response = mock.MagicMock()
-        mock_response.status_code = 200
-        mock_response.json.return_value = {'records': []}
-        mock_request.return_value = mock_response
+    @requests_mock.mock()
+    def test_get_only_one_field(self, mock_requests):
+        mock_requests.get(
+            'https://api.airtable.com/v0/app12345/TableName?fields=Name&fields=Name',
+            request_headers={'Authorization': 'Bearer fake_api_key'},
+            json={'records': []})
 
         self.get(fields=['Name'])
-        mock_request.assert_called_once_with(
-            'GET', 'https://api.airtable.com/v0/app12345/TableName',
-            data=None, headers={'Authorization': 'Bearer fake_api_key'},
-            params={'fields': ['Name', 'Name']})
+        self.assertEqual(1, mock_requests.called)
 
-    @mock.patch.object(requests, 'request')
-    def test_get_filter_by_formula(self, mock_request):
-        mock_response = mock.MagicMock()
-        mock_response.status_code = 200
-        mock_response.json.return_value = {'records': []}
-        mock_request.return_value = mock_response
+    @requests_mock.mock()
+    def test_get_filter_by_formula(self, mock_requests):
+        mock_requests.get(
+            "https://api.airtable.com/v0/app12345/TableName?filterByFormula=%7Btitle%7D+%3D+%27%27",
+            request_headers={'Authorization': 'Bearer fake_api_key'},
+            json={'records': []})
 
         self.get(filter_by_formula="{title} = ''")
-        mock_request.assert_called_once_with(
-            'GET', 'https://api.airtable.com/v0/app12345/TableName',
-            data=None, headers={'Authorization': 'Bearer fake_api_key'},
-            params={'filterByFormula': "{title} = ''"})
+        self.assertEqual(1, mock_requests.called)
 
     def test_invalid_get(self):
         with self.assertRaises(airtable.IsNotString):
@@ -194,15 +172,15 @@ class TestAirtable(unittest.TestCase):
         with self.assertRaises(airtable.IsNotInteger):
             self.get(limit='1')
 
-    @mock.patch.object(requests, 'request')
-    def test_delete(self, mock_request):
-        mock_response = mock.MagicMock()
-        mock_response.status_code = 200
-        mock_response.json.return_value = {
-            'deleted': True,
-            'id': '1234'
-        }
-        mock_request.return_value = mock_response
+    @requests_mock.mock()
+    def test_delete(self, mock_requests):
+        mock_requests.delete(
+            'https://api.airtable.com/v0/app12345/TableName/1234',
+            request_headers={'Authorization': 'Bearer fake_api_key'},
+            json={
+                'deleted': True,
+                'id': '1234'
+            })
         response = self.delete('1234')
         self.assertTrue(response['deleted'])
         self.assertEqual(response['id'], '1234')
@@ -211,43 +189,42 @@ class TestAirtable(unittest.TestCase):
         with self.assertRaises(airtable.IsNotString):
             self.delete(123)
 
-    @mock.patch.object(requests, 'request')
-    def test_iterate(self, mock_request):
-        mock_response1 = mock.MagicMock()
-        mock_response1.status_code = 200
-        mock_response1.json.return_value = {
-            'records': [
-                {
-                    'id': 'reccA6yaHKzw5Zlp0',
-                    'fields': {
-                        'Name': 'John',
-                        'Number': '(987) 654-3210'
+    @requests_mock.mock()
+    def test_iterate(self, mock_requests):
+        mock_requests.get(
+            'https://api.airtable.com/v0/app12345/TableName',
+            json={
+                'records': [
+                    {
+                        'id': 'reccA6yaHKzw5Zlp0',
+                        'fields': {
+                            'Name': 'John',
+                            'Number': '(987) 654-3210'
+                        }
+                    },
+                    {
+                        'id': 'reccg3Kke0QvTDW0H',
+                        'fields': {
+                            'Name': 'Nico',
+                            'Number': '(123) 222-1131'
+                        }
                     }
-                },
-                {
-                    'id': 'reccg3Kke0QvTDW0H',
-                    'fields': {
-                        'Name': 'Nico',
-                        'Number': '(123) 222-1131'
-                    }
-                }
-            ],
-            'offset': 'reccg3Kke0QvTDW0H'
-        }
-        mock_response2 = mock.MagicMock()
-        mock_response2.status_code = 200
-        mock_response2.json.return_value = {
-            'records': [
-                {
-                    'id': 'reccA23fSERw5Zlp0',
-                    'fields': {
-                        'Name': 'Ron',
-                        'Number': '(987) 654-3210'
-                    }
-                },
-            ],
-        }
-        mock_request.side_effect = [mock_response1, mock_response2]
+                ],
+                'offset': 'reccg3Kke0QvTDW0H'
+            })
+        mock_requests.get(
+            'https://api.airtable.com/v0/app12345/TableName?offset=reccg3Kke0QvTDW0H',
+            json={
+                'records': [
+                    {
+                        'id': 'reccA23fSERw5Zlp0',
+                        'fields': {
+                            'Name': 'Ron',
+                            'Number': '(987) 654-3210'
+                        }
+                    },
+                ],
+            })
         results = list(self.iterate())
         self.assertEqual(
             ['John', 'Nico', 'Ron'], [r.get('fields', {}).get('Name') for r in results])
@@ -284,22 +261,21 @@ class TestTableFromConfig(TestAirtable):
     def iterate(self, *args, **kwargs):
         return self.table.iterate(*args, **kwargs)
 
-    @mock.patch.object(requests, 'request')
-    def test_typed_table(self, mock_request):
-        mock_response = mock.MagicMock()
-        mock_response.status_code = 200
-        mock_response.json.return_value = {
-            'records': [
-                {
-                    'id': 'reccA23fSERw5Zlp0',
-                    'fields': {
-                        'Name': 3,
-                        'Number': 4
-                    }
-                },
-            ],
-        }
-        mock_request.return_value = mock_response
+    @requests_mock.mock()
+    def test_typed_table(self, mock_requests):
+        mock_requests.get(
+            'https://api.airtable.com/v0/app12345/TableName',
+            json={
+                'records': [
+                    {
+                        'id': 'reccA23fSERw5Zlp0',
+                        'fields': {
+                            'Name': 3,
+                            'Number': 4
+                        }
+                    },
+                ],
+            })
 
         table = airtable.Table[Dict[str, int]](
             self.base_id, FAKE_TABLE_NAME, api_key=FAKE_API_KEY)
