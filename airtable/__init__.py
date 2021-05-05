6969dc7e6973b7ec277d1b9733843198c65cb630
Adam Mills <adam.mills@hatch.team>
2021-05-05 17:13:09 +1000

    moduke import

--- airtable/__init__.py
+++ airtable/__init__.py
@@ -5,7 +5,7 @@ from typing import Any, Generic, Mapping, TypeVar
 import warnings
 
 import requests
-from requests.packages.urllib3.util.retry import Retry
+from requests.packages.urllib3.util import retry
 import six
 
 API_URL = 'https://api.airtable.com/v%s/'
@@ -91,13 +91,11 @@ class Airtable(object):
         self.base_url = posixpath.join(self.airtable_url, base_id)
         self.headers = {'Authorization': 'Bearer %s' % api_key}
         self._dict_class = dict_class
-
     
-
     def __request(self, method, url, params=None, payload=None):
         if method in ['POST', 'PUT', 'PATCH']:
             self.headers.update({'Content-type': 'application/json'})
-        retries = Retry(total=3, backoff_factor=1, status_forcelist=[429, 500, 502, 503, 504])
+        retries = retry.Retry(total=3, backoff_factor=1, status_forcelist=[429, 500, 502, 503, 504])
         session = requests.Session()
         session.mount('https://', requests.adapters.HTTPAdapter(max_retries=retries))
         response = session.request(
